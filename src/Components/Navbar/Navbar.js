949e27605ad8aeb884656dd579573d76aefd4e9a
Ogwarf <ome504@hotmail.com>
2021-03-24 16:23:13 +0700

    Navbar CartItems

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,15 +4,19 @@ import MenuItems from "./MenuItem";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
-import { Badge, Paper, InputBase } from "@material-ui/core";
+import { Badge, Paper, InputBase, Popper, Button } from "@material-ui/core";
 import CloseIcon from "@material-ui/icons/Close";
+import CreateOutlinedIcon from "@material-ui/icons/CreateOutlined";
 import { makeStyles } from "@material-ui/core/styles";
 
 import profile from "../../assets/profile.jpg";
 
+import book from "../../assets/book01.png";
+
 function Navbar() {
   const [shoppingCart] = useState({ amount: 1 });
   const [isSearching, setisSearching] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,6 +32,13 @@ function Navbar() {
       marginLeft: theme.spacing(1),
       flex: 1,
     },
+    paper: {
+      border: "1px solid",
+      paddingTop: 20,
+      width: 350,
+      textAlign: "center",
+      backgroundColor: theme.palette.background.paper,
+    },
   }));
 
   const classes = useStyles();
@@ -39,6 +50,14 @@ function Navbar() {
       setisSearching(false);
     }
   }
+
+  const handleClick = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popper" : undefined;
+
   return (
     <nav className="NavbarItems">
       <h1 className="Navbar-Home">
@@ -70,6 +89,7 @@ function Navbar() {
           )}
           <div className="Search__box">
             <button
+              aria-describedby={id}
               onClick={() => handleMouseOnClick()}
               style={{ padding: "0", border: "none", background: "none" }}
             >
@@ -79,6 +99,7 @@ function Navbar() {
               badgeContent={shoppingCart.amount}
               color="primary"
               style={{ marginLeft: "1.5vw" }}
+              onClick={handleClick}
             >
               <ShoppingCartOutlinedIcon
                 style={{
@@ -88,6 +109,133 @@ function Navbar() {
                 }}
               />
             </Badge>
+            <Popper
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              placement="bottom-end"
+              disablePortal={true}
+              modifiers={{
+                flip: {
+                  enabled: false,
+                },
+                preventOverflow: {
+                  enabled: true,
+                  boundariesElement: "scrollParent",
+                },
+              }}
+            >
+              <div className={classes.paper}>
+                <h5 style={{ fontWeight: "bold", fontSize: "20px" }}>
+                  ตระกร้าของฉัน
+                </h5>
+                <p style={{ fontSize: "15px", color: "#9e9e9e" }}>
+                  {shoppingCart.amount} สินค้าในตระกร้า
+                </p>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  style={{
+                    borderRadius: "50px",
+                    borderWidth: "2px",
+                    paddingLeft: 50,
+                    paddingRight: 50,
+                    marginBottom: 10,
+                  }}
+                >
+                  ดูหรือแก้ไขสินค้าของฉัน
+                </Button>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    borderTop: "1.5px solid",
+                    borderBottom: "1.5px solid",
+                    paddingTop: 15,
+                    paddingBottom: 15,
+                  }}
+                >
+                  <p
+                    style={{
+                      flex: 1,
+                      textAlign: "right",
+                      paddingTop: "20px",
+                      paddingRight: "5px",
+                    }}
+                  >
+                    {shoppingCart.amount} x
+                  </p>
+                  <div
+                    style={{
+                      flex: 7,
+                      textAlign: "left",
+                      display: "flex",
+                      flexDirection: "row",
+                    }}
+                  >
+                    <img
+                      src={book}
+                      alt="img product"
+                      style={{
+                        height: "70px",
+                        width: "70px",
+                        marginRight: "10px",
+                      }}
+                    />
+                    <p>ตัวประกอบสะท้านยุทธภพ</p>
+                  </div>
+                  <div
+                    style={{
+                      flex: 2,
+                      display: "flex",
+                      flexDirection: "column",
+                      textAlign: "right",
+                    }}
+                  >
+                    <CloseIcon
+                      style={{
+                        height: "20px",
+                        border: "1px solid",
+                        borderRadius: "50%",
+                        marginBottom: "3px",
+                        cursor: "pointer",
+                      }}
+                    />
+                    <CreateOutlinedIcon
+                      style={{
+                        height: "20px",
+                        border: "1px solid",
+                        borderRadius: "50%",
+                        cursor: "pointer",
+                      }}
+                    />
+                  </div>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    textAlign: "center",
+                  }}
+                >
+                  <p style={{ paddingTop: 20, flex: 1 }}>
+                    ยอดรวม : <b>THB129.00</b>
+                  </p>
+                </div>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{
+                    borderRadius: "50px",
+                    paddingLeft: 50,
+                    paddingRight: 50,
+                    marginBottom: 10,
+                  }}
+                >
+                  ไปชำระเงิน
+                </Button>
+              </div>
+            </Popper>
             <img src={profile} alt="Avatar" className="avatar" />
           </div>
         </div>
